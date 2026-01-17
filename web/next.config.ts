d7ebe2291d82258c41805e5e42df8c846111605c
MacBookAir <macbookair@MacBookAirs-MacBook-Air.local>
2026-01-17 00:53:07 -0400

    Configure Next.js to ignore build errors

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   distDir: '../.next',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // @ts-expect-error - eslint is missing from NextConfig type
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
