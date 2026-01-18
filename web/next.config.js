cd15c03fa622d3f549b3c13522c135b350e78c0f
MacBookAir <macbookair@MacBookAirs-MacBook-Air.local>
2026-01-17 20:13:21 -0400

    Fix build script and update dependencies

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+ /* config options here */
   distDir: '../.next',
   typescript: {
     ignoreBuildErrors: true,
